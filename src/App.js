0ba39d1ebeecb4377e175969f11607e200ee3617
josephndegwa <kariukindegwa98@gmail.com>
2022-06-08 22:34:56 +0300

    add navigation component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/Allmeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupsPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
   <div> 
+    <MainNavigation />
     <Routes>
       <Route path="/" element={<AllMeetupsPage />} />
       <Route path="/new-meetup" element={<NewMeetupsPage/>} />
