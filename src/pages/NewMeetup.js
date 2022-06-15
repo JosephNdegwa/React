be1fc2ddd7c37312ada00456103b23edce861859
josephndegwa <kariukindegwa98@gmail.com>
2022-06-15 15:40:21 +0300

    navigatting programatically using useNavigate

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupsPage() {
+    const navigate = useNavigate();
+
     function addMeetupHandler(meetupData) {
         fetch(
             'https://react-d5c5e-default-rtdb.firebaseio.com/meetups.json',
@@ -11,7 +14,9 @@ function NewMeetupsPage() {
                     'Content-Type': 'application/json'
                 }
             }
-        );
+        ).then(() => {
+            navigate('/');
+        });
     }
 
     return (
