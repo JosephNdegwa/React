2817abb1d50e5cccc59fb6a61eff187904c75db4
josephndegwa <kariukindegwa98@gmail.com>
2022-06-15 20:53:47 +0300

    edit favorites page to display added favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,9 +5,17 @@ import MeetupList from '../components/meetups/MeetupList';
 function FavoritesPage() {
     const favoriteCtx = useContext(FavoritesContext);
 
+    let content;
+
+    if (favoriteCtx.totalFavorites === 0) {
+        content = <p>You have no favorites yet. Start adding some?</p>
+    } else {
+        content = <MeetupList meetups={favoriteCtx.favorites} />
+    }
+
     return <section>
         <h1>My Favorites</h1>
-        <MeetupList meetups={favoriteCtx.favorites} />
+        {content}
     </section>;
 }
 
